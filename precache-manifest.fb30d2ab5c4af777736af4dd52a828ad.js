4e8e0f7b9fd359098801ceaf3b3fc3ea27fd9a2a
lobik <lobikova12@mail.ru>
2020-06-27 21:47:19 +0300

    Updates

---
+++ precache-manifest.fb30d2ab5c4af777736af4dd52a828ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9b0b11267e4ef925cdbcb721fb9bc7",
+    "url": "/socialweb/index.html"
+  },
+  {
+    "revision": "f36d9b630ca88ae77656",
+    "url": "/socialweb/static/css/main.334835aa.chunk.css"
+  },
+  {
+    "revision": "ae588e1055e187945dce",
+    "url": "/socialweb/static/js/2.5c621155.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialweb/static/js/2.5c621155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36d9b630ca88ae77656",
+    "url": "/socialweb/static/js/main.69ff5276.chunk.js"
+  },
+  {
+    "revision": "b298a45f634a96f775a4",
+    "url": "/socialweb/static/js/runtime-main.ce7187fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/socialweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
